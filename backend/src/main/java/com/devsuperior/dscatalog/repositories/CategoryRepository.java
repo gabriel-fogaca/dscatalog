e255c6d05cda474b7d60dcf01a0170429a365d9a
gabriel-fogaca <l44.gabriel@hotmail.com>
2025-04-01 17:51:48 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.devsuperior.dscatalog.repositories;
 
-public class CategoryRepository {
+import com.devsuperior.dscatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
